6287b058fc442a9729ae497d0c02783b0d3022ab
Anderson Martins <atmmoreira.rj@gmail.com>
2020-10-07 23:01:27 -0300

    refactor: all code

--- src/app.js
+++ src/app.js
@@ -1,34 +1,23 @@
 import { fetchUrl } from './helpers/apiFetch';
 import { formatAmount } from './helpers/formatAmount';
+import { url, inputSalary, optionsCurrency, cotation, salaryDigited, salaryMonth, salaryConverted, currency, currency2 } from './helpers/selectors';
 
-const selectors = [
-  document.querySelector('#inputSalary'),
-  document.querySelector('#optionsCurrency'),
-  document.querySelector('#cotation'),
-  document.querySelector('#salaryDigited'),
-  document.querySelector('#salaryMonth'),
-  document.querySelector('#salaryConverted'),
-  document.querySelector('#currency'),
-  document.querySelector('#currency2'),
-  'https://economia.awesomeapi.com.br/json/all'
-];
-
-fetchUrl(selectors[8])
+fetchUrl(url)
   .then(response => response.json())
   .then(data => {
     for (const key in data) {
-      selectors[1].innerHTML += `
+      optionsCurrency.innerHTML += `
         <option value="${data[key].code}">${data[key].name}</option>
       `;
     }
 
     optionsCurrency.addEventListener('change', () => {
-      selectors[2].textContent = `${data[selectors[1].value].high}`;
-      selectors[6].textContent = `${selectors[1].value}`;
-      selectors[7].textContent = `${selectors[1].value}`;
-      selectors[3].textContent = formatAmount(selectors[0].value);
-      selectors[4].textContent = formatAmount(selectors[0].value) / 12;
-      selectors[5].textContent = formatAmount((selectors[0].value / 12) * data[selectors[1].value].high);
+      cotation.textContent = `${data[optionsCurrency.value].high}`;
+      currency.textContent = `${optionsCurrency.value}`;
+      currency2.textContent = `${optionsCurrency.value}`;
+      salaryDigited.textContent = formatAmount(inputSalary.value);
+      salaryMonth.textContent = (inputSalary.value / 12).toFixed(2);
+      salaryConverted.textContent = formatAmount((inputSalary.value / 12) * data[optionsCurrency.value].high);
     });
   })
   .catch(error => { console.error(error); });
---
+++ src/helpers/selectors.js
@@ -0,0 +1,13 @@
+let url = 'https://economia.awesomeapi.com.br/json/all';
+let inputSalary = document.querySelector('#inputSalary');
+let optionsCurrency = document.querySelector('#optionsCurrency');
+let cotation = document.querySelector('#cotation');
+let salaryDigited = document.querySelector('#salaryDigited');
+let salaryMonth = document.querySelector('#salaryMonth');
+let salaryConverted = document.querySelector('#salaryConverted');
+let currency = document.querySelector('#currency');
+let currency2 = document.querySelector('#currency2');
+
+export {
+  url, inputSalary, optionsCurrency, cotation, salaryDigited, salaryMonth, salaryConverted, currency, currency2
+}
